da3c37e0c7a3fb3eb6d1bfc272e0aa850a072986
Teshager <teshager8922@gmail.com>
2025-05-14 18:16:25 +0300

    explain in the index file

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -19,3 +19,9 @@ CREATE INDEX idx_location ON properties (location);
 -- Consider composite indexes for columns often used together in WHERE clauses
 -- Example: Finding bookings within a specific date range for a specific property
 CREATE INDEX idx_property_date_bookings ON bookings (property_id, booking_date);
+
+
+--performance
+EXPLAIN SELECT * FROM users WHERE username = 'some_username';
+EXPLAIN SELECT * FROM bookings WHERE user_id = 123 AND booking_date BETWEEN '2024-01-01' AND '2024-01-31';
+EXPLAIN SELECT p.property_name FROM properties p JOIN bookings b ON p.property_id = b.property_id WHERE b.user_id = 456;
