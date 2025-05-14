157cb27c6a8a5e3f5c373a03962c5b2c932dbecf
Teshager <teshager8922@gmail.com>
2025-05-14 16:43:15 +0300

    simulate full outerjoin for mysql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -26,13 +26,26 @@ LEFT JOIN
 
 --3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 
-SELECT
-    u.user_id AS user_id,
-    u.username,
-    b.booking_id,
-    b.booking_date
-FROM
-    users u
-FULL OUTER JOIN
-    bookings b ON u.user_id = b.user_id;
+SELECT 
+    users.user_id,
+    users.name,
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.booking_date
+FROM 
+    users
+LEFT JOIN 
+    bookings ON users.user_id = bookings.user_id
 
+UNION
+
+SELECT 
+    users.user_id,
+    users.name,
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.booking_date
+FROM 
+    bookings
+LEFT JOIN 
+    users ON bookings.user_id = users.user_id;
