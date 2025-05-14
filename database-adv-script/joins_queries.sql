5502a07aa8f942a800a5fcd19c34099e4f7a8961
Teshager <teshager8922@gmail.com>
2025-05-14 17:08:34 +0300

    add order by clause to the left join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -24,6 +24,7 @@ FROM
     properties p
 LEFT JOIN 
     reviews r ON p.property_id = r.property_id;
+ORDER BY p.property_id
 
 --3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 
