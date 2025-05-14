5008d78ce88b6122f6ace04d169599e0eb23619c
Teshager <teshager8922@gmail.com>
2025-05-14 16:29:38 +0300

    retrieve all properties and their values including those that have no review

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,4 +1,5 @@
 --1. INNER JOIN: Retrieve all bookings and the respective users who made those bookings.
+
 SELECT
     b.booking_id,
     b.booking_date,
@@ -9,3 +10,16 @@ FROM
     bookings b
 INNER JOIN
     users u ON b.user_id = u.user_id;
+
+--2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews.
+
+SELECT
+    p.property_id,
+    p.property_name,
+    r.review_id,
+    r.review_text,
+    r.rating
+FROM
+    properties p
+LEFT JOIN
+    reviews r ON p.property_id = r.property_id;
