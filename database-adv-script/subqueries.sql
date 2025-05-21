c2d0b16d46f7d4da0b0e11fa6399b6ada0c4c1ed
teshager <teshager8922@gmail.com>
2025-05-21 10:44:13 +0300

    refactor: write correlated and non-correlated mysql queries

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,27 +1,22 @@
---1. Non-Correlated Subquery: Find all properties where the average rating is greater than 4.0.
-SELECT
-    p.property_id,
-    p.property_name
-FROM
-    properties p
-WHERE
-    p.property_id IN (
-        SELECT
-            r.property_id
-        FROM
-            reviews r
-        GROUP BY
-            r.property_id
-        HAVING
-            AVG(r.rating) > 4.0
-    );
+-- 1. Non-Correlated Subquery
 
---2. Correlated Subquery: Find users who have made more than 3 bookings.
+-- Find all properties where the average rating is greater than 4.0
+SELECT *
+FROM Property
+WHERE property_id IN (
+    SELECT property_id
+    FROM Review
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
 
-SELECT
-    u.user_id,
-    u.username
-FROM
-    users u
-WHERE
-    (SELECT COUNT(*) FROM bookings b WHERE b.user_id = u.user_id) > 3;
+-- 2. Correlated Subquery
+
+-- Find users who have made more than 3 bookings
+SELECT *
+FROM User u
+WHERE (
+    SELECT COUNT(*)
+    FROM Booking b
+    WHERE b.user_id = u.user_id
+) > 3;
