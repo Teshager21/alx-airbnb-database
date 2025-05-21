bc81cbc201dacdf0146a972d824e25645fa4bf31
teshager <teshager8922@gmail.com>
2025-05-21 11:11:51 +0300

    refactor: apply mysql aggregation and window functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,50 +1,20 @@
---1. Find the total number of bookings made by each user using COUNT and GROUP BY.
-SELECT
+-- 1. Total Number of Bookings by Each User
+SELECT 
     u.user_id,
-    u.username,
+    u.first_name,
+    u.last_name,
     COUNT(b.booking_id) AS total_bookings
-FROM
-    users u
-LEFT JOIN
-    bookings b ON u.user_id = b.user_id
-GROUP BY
-    u.user_id,
-    u.username
-ORDER BY
-    total_bookings DESC;
+FROM User u
+LEFT JOIN Booking b ON u.user_id = b.user_id
+GROUP BY u.user_id, u.first_name, u.last_name;
 
---2. Rank properties based on the total number of bookings they have received using window functions (ROW_NUMBER and RANK).
+--  2. Rank Properties by Number of Bookings (Using RANK)
 
-WITH PropertyBookingCounts AS (
-    SELECT
-        p.property_id,
-        p.property_name,
-        COUNT(b.booking_id) AS total_bookings
-    FROM
-        properties p
-    LEFT JOIN
-        bookings b ON p.property_id = b.property_id
-    GROUP BY
-        p.property_id,
-        p.property_name
-)
-SELECT
-    property_id,
-    property_name,
-    total_bookings,
-    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS booking_rank_row_number
-FROM
-    PropertyBookingCounts
-ORDER BY
-    booking_rank_row_number;
-
---Using RANK():
-SELECT
-    property_id,
-    property_name,
-    total_bookings,
-    RANK() OVER (ORDER BY total_bookings DESC) AS booking_rank_rank
-FROM
-    PropertyBookingCounts
-ORDER BY
-    booking_rank_rank;
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM Property p
+LEFT JOIN Booking b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name;
